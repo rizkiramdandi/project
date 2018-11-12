41cd37b6cf18e7a152f77e6558205fc74aae276b
Rizki <43452155+rizkiramdandi@users.noreply.github.com>
2018-11-12 14:08:10 +0700

    Add files via upload

---
+++ skeleton.js
@@ -0,0 +1,35 @@
+ /* Let's Build: With JavaScript - dev-android.tech
+                 
+                 
+                 Let's Build: Skeleton Screens 
+              */
+
+var cardImage = document.querySelector('.card-image');
+var cardTitle = document.querySelector('.card-title');
+var cardDesc = document.querySelector('.card-description');
+
+
+var renderCard = function renderCard() {
+  cardTitle.textContent = 'Card Title Yo!';
+  cardDesc.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero dicta repellat quibusdam assumenda at distinctio similique eos? Officiis voluptatem accusamus, id odit, quos eaque nemo, dicta quidem eum eligendi veritatis.';
+  createImage();
+  cardTitle.classList.remove('loading');
+  cardDesc.classList.remove('loading');
+  cardImage.classList.remove('loading');
+};
+
+function createImage() {
+  var img = new Image();
+  img.classList.add("image");
+  img.setAttribute('alt', 'A cloud day at a mountain base');
+  img.setAttribute('src', 'https://images.unsplash.com/photo-1516646255117-f9f933680173?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=dc874984018a4253ba7d2a3c53387135');
+  cardImage.appendChild(img);
+}
+
+setTimeout(function () {
+  renderCard();
+}, 5000);
+
+// window.addEventListener('load', () => {
+//   renderCard();
+// });
